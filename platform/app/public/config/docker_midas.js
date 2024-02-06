bed1cf900e248231a05e39684fb778f0c0bbcd3d
Hemant Bothra <hbothra@alphasense.com>
2024-02-06 14:17:35 +0530

    Sync from release branch

---
+++ platform/app/public/config/docker_midas.js
@@ -0,0 +1,53 @@
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  extensions: [],
+  modes: [],
+  // below flag is for performance reasons, but it might not work for all servers
+
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  strictZSpacingForVolumeViewport: true,
+  defaultDataSourceName: 'dicomweb',
+  dataSources: [
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
+        friendlyName: 'Orthanc Server',
+        name: 'Orthanc',
+        wadoUriRoot: 'https://127.0.0.1/pacs/dicom-web',
+        qidoRoot: 'https://127.0.0.1/pacs/dicom-web',
+        wadoRoot: 'https://127.0.0.1/pacs/dicom-web',
+        qidoSupportsIncludeField: true,
+        supportsReject: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        supportsWildcard: true,
+        dicomUploadEnabled: true,
+        bulkDataURI: {
+          enabled: true,
+        },
+      },
+    },
+  ],
+  // This is an array, but we'll only use the first entry for now
+  oidc: [
+    {
+      // ~ REQUIRED
+      // Authorization Server URL
+      authority: 'https://127.0.0.1/kc/realms/midas',
+      client_id: 'ohif-viewer',
+      redirect_uri: 'https://127.0.0.1/callback', // `OHIFStandaloneViewer.js`
+      // "Authorization Code Flow"
+      // Resource: https://medium.com/@darutk/diagrams-of-all-the-openid-connect-flows-6968e3990660
+      response_type: 'code',
+      scope: 'openid', // email profile openid
+      // ~ OPTIONAL
+      post_logout_redirect_uri: '/logout-redirect.html',
+    },
+  ],
+};
