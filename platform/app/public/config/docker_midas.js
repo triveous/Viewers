f1d1c64269210c3b86635f7291825263f62df89a
Hemant Bothra <hbothra@alphasense.com>
2024-02-28 15:01:38 +0530

    BAse path changes

--- platform/app/public/config/docker_midas.js
+++ platform/app/public/config/docker_midas.js
@@ -3,6 +3,7 @@ window.config = {
   showStudyList: true,
   extensions: [],
   modes: [],
+  disableEditing: true,
   // below flag is for performance reasons, but it might not work for all servers
 
   showWarningMessageForCrossOrigin: true,
