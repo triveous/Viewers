f0f17042d490d422cc641f5a5aeffc5b1c8e7969
hemant-triveous <149148868+hemant-triveous@users.noreply.github.com>
2024-08-05 06:35:25 +0530

    Update docker_midas.js
    Adding changes needed to work OHIF with dcm4chee server

--- platform/app/public/config/docker_midas.js
+++ platform/app/public/config/docker_midas.js
@@ -15,22 +15,10 @@ window.config = {
   maxNumRequests: {
     interaction: 100,
     thumbnail: 75,
-    // Prefetch number is dependent on the http protocol. For http 2 or
-    // above, the number of requests can be go a lot higher.
     prefetch: 25,
   },
   filterQueryParam: false,
   defaultDataSourceName: 'dicomweb',
-  /* Dynamic config allows user to pass "configUrl" query string this allows to load config without recompiling application. The regex will ensure valid configuration source */
-  // dangerouslyUseDynamicConfig: {
-  //   enabled: true,
-  //   // regex will ensure valid configuration source and default is /.*/ which matches any character. To use this, setup your own regex to choose a specific source of configuration only.
-  //   // Example 1, to allow numbers and letters in an absolute or sub-path only.
-  //   // regex: /(0-9A-Za-z.]+)(\/[0-9A-Za-z.]+)*/
-  //   // Example 2, to restricts to either hosptial.com or othersite.com.
-  //   // regex: /(https:\/\/hospital.com(\/[0-9A-Za-z.]+)*)|(https:\/\/othersite.com(\/[0-9A-Za-z.]+)*)/
-  //   regex: /.*/,
-  // },
   dataSources: [
     {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
@@ -50,9 +38,26 @@ window.config = {
         supportsWildcard: true,
         dicomUploadEnabled: false,
         acceptHeader: ['*/*'],
+        singlepart: 'pdf,video',
+        omitQuotationForMultipartRequest: true,
         bulkDataURI: {
           enabled: true,
         },
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
+      sourceName: 'dicomjson',
+      configuration: {
+        friendlyName: 'dicom json',
+        name: 'json',
+      },
+    },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
+      sourceName: 'dicomlocal',
+      configuration: {
+        friendlyName: 'dicom local',
       },
     },
   ],
